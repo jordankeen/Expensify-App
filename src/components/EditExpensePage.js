7570d08d89571824d34e60795b669cbe992ac8a5
Jordan Keen <jordankeen.7@gmail.com>
2018-10-06 11:08:47 -0400

    import redux

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-	<div>
-		edit expense (3) editzz
-	</div>
-);
+const EditExpensePage = (props) => {
+	console.log(props);
+	return (
+		<div>
+			Editin Expense with id of {props.match.params.id}
+		</div>
+	);
+};
 
 export default EditExpensePage;
