bab4d8d7384a677bc1c5268b44a6862acd78e67c
Jordan Keen <jordankeen.7@gmail.com>
2018-10-05 20:37:44 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// import react
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(<p>boilerplate</p>, document.getElementById('app'));
+
+
+
+
+
