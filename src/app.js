bfdf8ad3027af48e7220c3c4cbf77be5cca1414b
Jordan Keen <jordankeen.7@gmail.com>
2018-10-05 22:19:20 -0400

    nav setup, component file structure
    - import Switch, Link, NavLink
    - setup of navigation
    - created component folder

--- src/app.js
+++ src/app.js
@@ -1,43 +1,9 @@
 // import react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-	<div>
-		from dashboard (1)
-	</div>
-);
-const AddExpensePage = () => (
-	<div>
-		Add Expense Component (2)
-	</div>
-);
-const EditExpensePage = () => (
-	<div>
-		edit expense (3)
-	</div>
-);
-const HelpPage = () => (
-	<div>
-		Help Page (4)
-	</div>
-);
-const routes = (
-	<BrowserRouter>
-		<div>
-			<Route path="/" component={ExpenseDashboardPage} exact={true}/>
-			<Route path="/create" component={AddExpensePage} />
-			<Route path="/edit" component={EditExpensePage} />
-			<Route path="/help" component={HelpPage} />
-		</div>
-	</BrowserRouter>
-);
-ReactDOM.render(routes, document.getElementById('app'));
-
-
-
-
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpenseDashboardPage = () => (
+	<div>
+		from dashboard (1) home!
+	</div>
+);
+
+export default ExpenseDashboardPage;
